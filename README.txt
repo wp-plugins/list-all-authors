e415c0e9d01d724ce301c9184f124839d0047081
NickRita <NickRita@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-29 06:58:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/list-all-authors/trunk@48339 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,80 @@
+=== Wordpress Plugin List-all-authors ===
+Contributors: Andrea Heinrich
+Donate link: http://www.song-line.de/2008/05/25/plugin-list-all-authors/
+Tags: author list
+Requires at least: 1.5
+Tested up to: 2.3
+Stable tag: 1.0
+
+
+== Change Log ==
+1.0 initial release
+
+
+Englisch Translation below.
+
+
+
+Ich habe es geschafft! Mein erstes Plugin ist fertig und es läuft. 
+
+== Description ==
+
+Bei Wordpress gibt es das Tag "wp-list-authors", mit dem sich eine Liste aller Autoren aufrufen lässt. Dieses Liste beinhaltet jedoch nur Autoren, die bereits einen Beitrag verfasst haben. 
+
+Wer in einem Multi-User-Blog eine Liste aller User erstellen möchte, also auch solcher, die noch keinen Beitrag verfasst haben, kann dies mit dem Plugin "List all authors".
+
+== Installation ==
+
+1. Plugin runterladen, extrahieren und in den Ordner wp-content/plugins hochladen.
+2. Plugin aktivieren.
+3. Die Liste von Autoren an der gewünschten Stelle (Sidebar oder besondere Seite) mit folgender Funktion aufrufen: 
+
+<?php if (function_exists('wp_all_authors')) { ?><?php wp_all_authors('show_fullname=0&optioncount=1&hide_empty=0&exclude_admin=0'); ?><?php } ?>
+
+Diese Funktion beinhaltet folgende Parameter:
+
+optioncount=1   Anzahl der Artikel wird angezeigt. Falls dies nicht gewünscht ist, statt der 1 eine 0 setzen.
+
+hide_empty=0  Auch User ohne Beiträge werden angezeigt. Falls nur User mit Beiträgen angezeigt werden sollen, hier eine 1 setzen.
+
+exclude_admin=0  Der Administrator wird in der Liste angezeigt. Falls dies nicht gewünscht ist, statt der 0 eine 1 setzen.
+
+Es können alle Parameter der Wordpress-Funktion <a href="http://codex.wordpress.org/Template_Tags/wp_list_authors"target="_blank">wp-list-authors</a> genutzt werden.
+
+Fertig!
+Das Plugin steht kostenfrei zur Verfügung. Falls es Dir gefällt, schenk mir einen Kommentar.
+
+
+
+For the english-speaking friends:
+
+I got it! My first wordpress-plugin runs!
+
+== Description ==
+
+Wordpress has the tag "wp-list-authors" to get a list of all authors. This list includes only those authors who already published a post. 
+
+Anyone who wants to have a list of users with AND without posts, for exmple in multi-user-blogs, can have it now with my plugin.
+
+== Installation ==
+
+1. Download Plugin, unzip it and upload it to the to the root of your Wordpress "plugins" folder (should be file wp-content/plugins).
+2. Acivate it on the admin panel. 
+3. You get the list of authors wherever you want (sidebar or seperate page) by using the following code:
+
+<?php if (function_exists('wp_all_authors')) { ?><?php wp_all_authors('show_fullname=0&optioncount=1&hide_empty=0&exclude_admin=0'); ?><?php } ?>
+
+This function includes:
+
+optioncount=1   The number of posts of each author is shown. If you don't want this, write 0 instead of 1. 
+
+hide_empty=0  Users without posts are shown. If you only want to show users with posts, use an 1 instead of 0:
+
+exclude_admin=0  The administrator of the blog is shown. If you don't want this, use the 1 instead of 0.
+
+You can use all parameters of wordpress-function <a href="http://codex.wordpress.org/Template_Tags/wp_list_authors"target="_blank">wp-list-authors</a>.
+
+That's it!
+The plugin is for free use. If you like it, don't give money, but a comment ;-)
+
+Enjoy!
---
+++ list-all-authors.php
@@ -0,0 +1,112 @@
+<?php
+/*
+Plugin Name: List-all-authors
+Plugin URI: http://www.song-line.de/2008/05/25/plugin-list-all-authors/
+Description: Das Plugin ermoeglicht die Auflistung aller Authoren, auch solcher, die noch keine Artikel geschrieben haben. The plugin lets you lists all authors, even those without posts.
+Author: Andrea Heinrich
+Version: 1.0
+Author URI: http://www.song-line.de
+
+
+ This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+*/
+
+/**
+ * List all the authors of the blog, with several options available.
+ * optioncount (boolean) (false): Show the count in parenthesis next to the author's name.
+ * exclude_admin (boolean) (true): Exclude the 'admin' user that is installed by default.
+ * show_fullname (boolean) (false): Show their full names.
+ * hide_empty (boolean) (false): Show authors without any posts.
+ * feed (string) (''): If isn't empty, show links to author's feeds.
+ * feed_image (string) (''): If isn't empty, use this image to link to feeds.
+ * echo (boolean) (true): Set to false to return the output, instead of echoing.
+ * @param array $args The argument array.
+ * @return null|string The output, if echo is set to false.
+ */
+function wp_all_authors($args = '') {
+	global $wpdb;
+
+	$defaults = array(
+		'optioncount' => false, 'exclude_admin' => true,
+		'show_fullname' => false, 'hide_empty' => true,
+		'feed' => '', 'feed_image' => '', 'echo' => true
+	);
+
+	$r = wp_parse_args( $args, $defaults );
+	extract($r, EXTR_SKIP);
+
+	$return = '';
+
+	// TODO:  Move select to get_authors().
+	$authors = $wpdb->get_results("SELECT ID, user_nicename from $wpdb->users " . ($exclude_admin ? "WHERE user_login <> 'admin' " : '') . "ORDER BY display_name");
+
+	$author_count = array();
+	foreach ((array) $wpdb->get_results("SELECT DISTINCT post_author, COUNT(ID) AS count FROM $wpdb->posts WHERE post_type = 'post' AND " . get_private_posts_cap_sql( 'post' ) . " GROUP BY post_author") as $row) {
+		$author_count[$row->post_author] = $row->count;
+	}
+
+	foreach ( (array) $authors as $author ) {
+		$author = get_userdata( $author->ID );
+		$posts = (isset($author_count[$author->ID])) ? $author_count[$author->ID] : 0;
+		$name = $author->display_name;
+
+		if ( $show_fullname && ($author->first_name != '' && $author->last_name != '') )
+			$name = "$author->first_name $author->last_name";
+
+		if ( !($posts == 0 && $hide_empty) )
+			$return .= '<li>';
+		if ( $posts == 0 ) {
+			if ( !$hide_empty )
+				$link = '<a href="' . get_author_posts_url($author->ID, $author->user_nicename) . '" title="' . sprintf(__("Posts by %s"), attribute_escape($author->display_name)) . '">' . $name . '</a>';
+		} else {
+			$link = '<a href="' . get_author_posts_url($author->ID, $author->user_nicename) . '" title="' . sprintf(__("Posts by %s"), attribute_escape($author->display_name)) . '">' . $name . '</a>';
+
+			if ( (! empty($feed_image)) || (! empty($feed)) ) {
+				$link .= ' ';
+				if (empty($feed_image))
+					$link .= '(';
+				$link .= '<a href="' . get_author_rss_link(0, $author->ID, $author->user_nicename) . '"';
+
+				if ( !empty($feed) ) {
+					$title = ' title="' . $feed . '"';
+					$alt = ' alt="' . $feed . '"';
+					$name = $feed;
+					$link .= $title;
+				}
+
+				$link .= '>';
+
+				if ( !empty($feed_image) )
+					$link .= "<img src=\"$feed_image\" border=\"0\"$alt$title" . ' />';
+				else
+					$link .= $name;
+
+				$link .= '</a>';
+
+				if ( empty($feed_image) )
+					$link .= ')';
+			}
+
+			if ( $optioncount )
+				$link .= ' ('. $posts . ')';
+
+		}
+
+		if ( !($posts == 0 && $hide_empty) )
+			$return .= $link . '</li>';
+	}
+	if ( !$echo )
+		return $return;
+	echo $return;
+}
+
+?>
